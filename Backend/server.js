1f493f4d65133781841f2ffd0a1285eb9f8ca326
Aravindkumar-06 <aravind191316112@gmail.com>
2024-08-02 19:52:37 +0530

    Added files

---
+++ Backend/server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions))
+
+app.get('/bfhl',(req,res)=>{
+    res.send({ operation_code: 1 })
+})
+
+
+app.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const userId = "john_doe_17091999"; 
+    const email = "john@xyz.com";       
+    const rollNumber = "ABCD123";     
+
+    let numbers = [];
+    let alphabets = [];
+    let highestAlphabet = '';
+
+
+    data.forEach(item => {
+        if (!isNaN(item)) {
+            numbers.push(item);
+        } else {
+            alphabets.push(item);
+            if (!highestAlphabet || item.toLowerCase() > highestAlphabet.toLowerCase()) {
+                highestAlphabet = item;
+            }
+        }
+    });
+
+    
+    const response = {
+        is_success: true,
+        user_id: userId,
+        email: email,
+        roll_number: rollNumber,
+        numbers: numbers,
+        alphabets: alphabets,
+        highest_alphabet: highestAlphabet ? [highestAlphabet] : []
+    };
+
+    res.json(response);
+});
+
+
+
+app.listen(3001, ()=>{
+    console.log('Server is running at port 3000')
+})
